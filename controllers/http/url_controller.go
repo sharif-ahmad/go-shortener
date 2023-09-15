ff38f7b514e3c996116120627acf503d6ef36d74
Sharif Ahmad <sharifrah@gmail.com>
2023-09-15 08:45:09 +0200

    rename shorten_url to create_short_url_command
    also fix indextation in few files

--- controllers/http/url_controller.go
+++ controllers/http/url_controller.go
@@ -1,33 +1,33 @@
 package http
 
 import (
-  "go-shortener/commands"
-  "go-shortener/repositories"
-  "net/http"
+	"go-shortener/commands"
+	"go-shortener/repositories"
+	"net/http"
 )
 
 type URLController struct {
-  BaseController
-  BaseUrl string
-  urlRepo repositories.URLRepository
+	BaseController
+	BaseUrl string
+	urlRepo repositories.URLRepository
 }
 
 func NewURLController(
-  baseUrl string, resp http.ResponseWriter, req *http.Request, urlRepo repositories.URLRepository,
+	baseUrl string, resp http.ResponseWriter, req *http.Request, urlRepo repositories.URLRepository,
 ) *URLController {
-  return &URLController{BaseController{resp, req}, baseUrl, urlRepo}
+	return &URLController{BaseController{resp, req}, baseUrl, urlRepo}
 }
 
 func (c *URLController) Put() {
-  url := c.GetQueryParams("url")
-  shortenUrlCmd := commands.NewShortenURL(url, c.urlRepo)
+	url := c.GetQueryParams("url")
+	shortenUrlCmd := commands.NewCreateShortURLCommand(url, c.urlRepo)
 
-  hash, err := shortenUrlCmd.Call()
-  if err != nil {
-    c.HandleError(err)
-    return
-  }
+	hash, err := shortenUrlCmd.Call()
+	if err != nil {
+		c.HandleError(err)
+		return
+	}
 
-  c.resp.WriteHeader(http.StatusOK)
-  c.resp.Write([]byte(c.BaseUrl + "/u/" + hash))
+	c.resp.WriteHeader(http.StatusOK)
+	c.resp.Write([]byte(c.BaseUrl + "/u/" + hash))
 }
